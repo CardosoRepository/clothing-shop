5cca7d664fd33fdc13ddaf9cdaa1359b83408aa4
Gabriel Cardoso Barbosa <gabrielcard0s0@hotmail.com>
2024-06-05 15:48:34 -0300

    bug: build error in the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from "./components/Header.jsx";
 import Shop from "./components/Shop.jsx";
 import { DUMMY_PRODUCTS } from "./dummy-products.js";
-import Product from "./components/product";
+import Product from "./components/product.jsx";
 import { CartContextProvider } from "./store/shopping-cart-context.jsx";
 
 function App() {
