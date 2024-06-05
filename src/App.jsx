c1f05930a8f91e65b47ef346d78045df298c250c
Gabriel Cardoso Barbosa <gabrielcard0s0@hotmail.com>
2024-06-05 15:50:16 -0300

    bug: correction for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import Header from "./components/Header.jsx";
 import Shop from "./components/Shop.jsx";
 import { DUMMY_PRODUCTS } from "./dummy-products.js";
-import Product from "./components/product.jsx";
+import Product from "./components/Product.jsx";
 import { CartContextProvider } from "./store/shopping-cart-context.jsx";
 
 function App() {
-    
-
     return (
         <CartContextProvider>
             <Header />
